d498bc11719ae1532a046684615fac7c1fc487f9
Bryton Junior <brytonkamte@gmail.com>
2026-05-19 18:08:43 +0200

    Aggiunto_Prova_StackJHC

---
+++ web corso/Martedi_19_Maggio/pomeriggio/EsempioJHC/main.js
@@ -0,0 +1,11 @@
+function cambiaTesto(){
+   // Seleziona l'elemento tramite id
+
+    let elemento = document.getElementById("testo");
+
+    
+
+    // Modifica il contenuto
+
+    elemento.textContent = "Testo modificato!";
+}
